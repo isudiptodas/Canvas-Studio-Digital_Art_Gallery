e91a49023c3f50549a5e8dee65e02baf41df8701
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-14 18:19:19 +0530

    added initial code

---
+++ nodejs-backend/arcjet/arcjet-protect.js
@@ -0,0 +1,31 @@
+import arcjet, { shield, detectBot, tokenBucket, validateEmail } from "@arcjet/node";
+
+const arcjetProtect = arcjet({
+    key: process.env.ARCJET_KEY,
+    rules: [
+        shield({ mode: "LIVE" }),
+        detectBot({
+            mode: "LIVE",
+            allow: [
+                "CATEGORY:SEARCH_ENGINE",
+                "CATEGORY:PREVIEW",
+            ]
+        }),
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 5,
+            interval: 10,
+            capacity: 5
+        }),
+    ]
+});
+
+const arcValidateEmail = arcjet({
+    key: process.env.ARCJET_KEY,
+    rules: [
+        validateEmail({
+            mode: "LIVE",
+            deny: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"]
+        })
+    ]
+});
