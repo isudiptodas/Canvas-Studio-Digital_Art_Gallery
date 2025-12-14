79f5b8cf9463b7e6057750019c76b5093585e538
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-14 21:02:03 +0530

    updated email validation route

--- nodejs-backend/routes/validateEmail.js
+++ nodejs-backend/routes/validateEmail.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import axios from 'axios';
+import { arcjetProtect } from '../arcjet/arcjet-protect.js';
+import { isSpoofedBot } from "@arcjet/inspect";
+
 
 const router = express.Router();
 
 router.post('/api/validate-email', async (req, res) => {
+    const decision = await arcjetProtect.protect(req);
+    if(decision.isDenied()){
+        return res.status(403).json({
+            message: `Forbidden`
+        });
+    }
+
+    if(decision.results.some(isSpoofedBot)){
+        return res.status(403).json({
+            message: `Forbidden`
+        });
+    }
+    
     const { email } = req.body;
 
     const API = process.env.ABSTRACT_API_KEY;
