a24f2669e2a420ab191fc6afc533ec3a4748bae6
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-31 21:57:29 +0530

    Update login.tsx

--- expo-frontend/app/(auth)/login.tsx
+++ expo-frontend/app/(auth)/login.tsx
@@ -1,1 +1,16 @@
 
+import { Text, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Login page</Text>
+    </View>
+  );
+}
