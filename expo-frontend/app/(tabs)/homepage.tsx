7df9028d38edbe7e95efd15760a4de1a71ff1516
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-01 00:20:05 +0530

    Create homepage.tsx

---
+++ expo-frontend/app/(tabs)/homepage.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+export default function Homepage() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home page.</Text>
+    </View>
+  );
+}
