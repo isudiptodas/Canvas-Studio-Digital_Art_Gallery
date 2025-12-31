4c6ce8babeae9d29890b9c1a612e81aa6b2fdabf
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-31 22:01:24 +0530

    Create emailVerification.tsx

---
+++ expo-frontend/app/(auth)/emailVerification.tsx
@@ -0,0 +1,16 @@
+
+import { Text, View } from "react-native";
+
+export default function EmailVerification() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Email verification page</Text>
+    </View>
+  );
+}
