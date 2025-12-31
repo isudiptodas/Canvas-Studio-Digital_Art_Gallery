47a908abcb40ec9662f8c838a56ab9b78969b870
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-01 00:23:38 +0530

    Create userProfile.tsx

---
+++ expo-frontend/app/(tabs)/userProfile.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+export default function UserProfile() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Profile page.</Text>
+    </View>
+  );
+}
